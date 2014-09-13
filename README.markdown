5127dcc40aa802ab625b5a1042b0488a1fa042c6
Martin Pärtel <martin.partel@gmail.com>
2014-09-13 13:18:46 +0100

    Added include/firewall-utils.bash.

--- README.markdown
+++ README.markdown
@@ -10,3 +10,8 @@ Transforms [eggdrop](http://www.eggheads.org/) IRC log files to HTML.
 
 Creates a user account on a remote host, and copies over their local
 `.ssh/authorized_keys`, if it exists, or generates a random password if not.
+
+## `include/firewall-utils.bash` ##
+
+Helper functions to include in `/etc/rc.local` or similar to set up
+simple port forwarding with iptables.
---
+++ include/firewall-utils.bash
@@ -0,0 +1,29 @@
+# Usage example:
+# clear_firewall
+# init_forwarding
+# forward_port 123 foo 456    # source-port  dest-host  dest-port
+
+gethost() {
+  getent hosts $1 | awk '{ print $1 }'
+}
+
+# Usage: forward_port source-port dest-host dest-port
+forward_port() {
+  SOURCE_PORT="$1"
+  DEST_IP=`gethost "$2"`
+  DEST_PORT="$3"
+
+  iptables -t nat -A PREROUTING -p tcp --dport $SOURCE_PORT -j DNAT --to $DEST_IP:$DEST_PORT
+  iptables -A FORWARD -p tcp -d $DEST_IP --dport $DEST_PORT -j ACCEPT
+}
+
+clear_firewall() {
+  iptables -F
+  iptables -t nat -F
+  echo 0 > /proc/sys/net/ipv4/ip_forward
+}
+
+init_forwarding() {
+  echo 1 > /proc/sys/net/ipv4/ip_forward
+  iptables -t nat -A POSTROUTING -j MASQUERADE
+}
